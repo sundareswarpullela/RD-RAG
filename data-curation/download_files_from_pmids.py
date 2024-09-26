ecc6ed64d47553bfd0d42aee82923aa53a8813c4
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2024-09-26 16:11:20 -0700

    parallelize pubmed file download

--- data-curation/download_files_from_pmids.py
+++ data-curation/download_files_from_pmids.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 import requests
 import xml.etree.ElementTree as ET
+import urllib.request
+from concurrent.futures import ThreadPoolExecutor
+
 
 
 #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8658462/
@@ -13,10 +16,14 @@ def download_file_from_pmc(pmc_id):
     locator_response = requests.get(oa_url)
     root = ET.fromstring(locator_response.text) 
     pdf_link = root.find(".//link[@format='pdf']")
-    print(pdf_link)
     if pdf_link is not None:
         pdf_url = pdf_link.get('href')
         print(f"PDF URL: {pdf_url}")
+        file_location = f"data-curation/data/files/{pmc_id}.pdf"
+
+
+        urllib.request.urlretrieve(pdf_url, file_location)
+        print(f"Downloaded {pdf_file_name}")
     else:
         print("PDF link not found")
 
@@ -34,11 +41,16 @@ for csv_file in csv_files:
     all_pmcids.update(pmcids)
 
 print(len(all_pmcids))
+print(len(set(all_pmcids)))
 
 all_pmcids = list(all_pmcids)
 print(all_pmcids[:5])
 
-for pmcid in all_pmcids:
-    print(pmcid)
-    download_file_from_pmc(pmcid)
-    break
+def download_multiple_pmc_files(pmc_ids):
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_file_from_pmc, pmc_ids)
+
+# List of PMC IDs you want to download
+
+# Run the download in parallel
+# download_multiple_pmc_files(all_pmcids[:100])

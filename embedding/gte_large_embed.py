12fcd313d136aee317b33c1879c74dba58782b76
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2025-01-25 17:13:49 -0800

    add trust remote code for gte

--- embedding/gte_large_embed.py
+++ embedding/gte_large_embed.py
@@ -6,8 +6,8 @@ from transformers import AutoModel, AutoTokenizer
 
 class GTEEmbedder:
     def __init__(self, model_path ="Alibaba-NLP/gte-large-en-v1.5"):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.model = AutoModel.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
 
 
     def __embed_text__(self, text):
